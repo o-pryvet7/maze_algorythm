34df0a9e7fec112048f46228b2e830e687f2544c
maksy <maksym.kosyk@gmail.com>
2022-07-22 16:10:03 +0300

    v1.2

--- src/main/java/maze/FindPathInFile.java
+++ src/main/java/maze/FindPathInFile.java
@@ -2,6 +2,7 @@ package maze;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
@@ -10,11 +11,15 @@ public class FindPathInFile extends AbstractFindPathInputReader {
     private String FileName;
     //private String temp_imp_maze;
     private char[][] _maze;
+    private int[][] _maze_index;// -1-target, 0-free way, 1-a free path that has already been used, 2-start, 3-wall,
     private int height=0,width=0;
     private int start_poz_x=0, start_poz_y=0, target_poz_x=0, target_poz_y=0;
 
     public void Run(){
         SizeAnalysis();
+        if(FileEquals("src/main/resources/previous_maze.txt" ,GetFileName())){
+            System.out.println("MAZES IS EQUALS");
+        } else System.out.println("MAZES ISN`T EQUALS");
         ReadFile();
     }
 
@@ -22,17 +27,47 @@ public class FindPathInFile extends AbstractFindPathInputReader {
 
     }
 
+    public boolean FileEquals(String Writer, String Reader){
+        boolean temp_return = true;
+        try{
+            FileReader fileReader = new FileReader(Reader);
+            FileReader fileWriter = new FileReader(Writer);
+            int chars1 = fileReader.read();
+            int chars2 = fileWriter.read();
+            System.out.println("Start Check File Equals");
+            if(chars1!=-1 && chars2!=-1)
+                while(chars1!=-1 || chars2!=-1){
+                    if(chars1!=chars2){
+                        temp_return = false;
+                    }
+                    chars1 = fileReader.read();
+                    chars2 = fileWriter.read();
+                }
+            fileWriter.close();
+            fileReader.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return temp_return;
+    }
+
     public void ReadFile(){
         int x=0,y=0;
         try{
             FileReader fileReader = new FileReader(FileName);
+            FileWriter fileWriter_maze = new FileWriter(new File("src/main/resources/previous_maze.txt"));
             int chars = fileReader.read();
             System.out.println("Start Read");
-            while(chars != -1){
-                System.out.print("Read is ok, curret x is: "+x+", curret y is: "+y+" curren chars is :");
+            if(chars!=-1) while(chars != -1){
+                fileWriter_maze.write((char)chars);
+                //System.out.print("\nRead is ok, curret x is: "+x+", curret y is: "+y+",curret char symbol is :"+chars+", curren chars is :");
                 _maze[x][y]=(char)chars;
-                if(_maze[x][y]=='\n'){
+                if((_maze[x][y]==13)||_maze[x][y]=='\n'){// CRLF
                     x=0; y++;
+                    chars = fileReader.read();
+                    fileWriter_maze.write((char)chars);
+                    _maze[x][y]=(char)chars;
                 }
                 else if(_maze[x][y]=='S'){
                     start_poz_x=x;
@@ -46,6 +81,9 @@ public class FindPathInFile extends AbstractFindPathInputReader {
                 x++;
                 chars = fileReader.read();
             }
+            else
+                System.out.println("\nERROR");
+            fileWriter_maze.close();
             fileReader.close();
         }
         catch(IOException e){
